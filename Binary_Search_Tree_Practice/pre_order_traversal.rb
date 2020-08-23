162ac796944e256e45e648dc773121d59d813f6d
Philip Huang <philip.sw.huang@gmail.com>
2020-08-22 19:23:33 -0700

    Created Binary Search Tree Practice Folder, started with pre-order traversal

---
+++ Binary_Search_Tree_Practice/pre_order_traversal.rb
@@ -0,0 +1,31 @@
+#################### BINARY TREE PREORDER TRAVERSAL ####################
+
+# Given a binary tree, return the preorder traversal of its nodes' values.
+
+# Example:
+# Input: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+# Output: [1,2,3]
+
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+#################### SOLUTION ####################
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+    
+end
