eb8d87f3720e368493c9578599eb776f6b4d18fb
Philip Huang <philip.sw.huang@gmail.com>
2020-08-25 18:48:56 -0700

    Updated Merge Two Sorted Lists with Recursive Solution

--- merge_two_sorted_lists.rb
+++ merge_two_sorted_lists.rb
@@ -19,6 +19,8 @@
 # @param {ListNode} l1
 # @param {ListNode} l2
 # @return {ListNode}
+
+# Iterative solution
 def merge_two_lists(l1, l2)
     return [] if (l1.nil? && l2.nil?)
     return l1 if (l2.nil?)
@@ -30,41 +32,45 @@ def merge_two_lists(l1, l2)
     while (!l1.nil? && !l2.nil?)
         if (l1.val <= l2.val)
             node.val = l1.val
-            node.next = ListNode.new
             l1 = l1.next
         else
             node.val = l2.val
-            node.next = ListNode.new()
             l2 = l2.next
         end
-        if (!l1.nil? && !l2.nil?)
-            node.next = ListNode.new
-        end            
+        node.next = ListNode.new            
         node = node.next
     end
     
     while(l1.nil? && !l2.nil?) 
         node.val = l2.val
-        if l2.next != nil
-            node.next = ListNode.new(l2.next)
-            node.next.val = l2.next.val
-        end
         l2 = l2.next
         if (!l2.nil?)
             node.next = ListNode.new
+            node = node.next
         end
-        node = node.next
     end
+    
     while(!l1.nil? && l2.nil?)
         node.val = l1.val
-        if l1.next != nil
-            node.next = ListNode.new(l1.next)
-        end
         l1 = l1.next
-        if (!l1.nil?)
+        if(!l1.nil?)
             node.next = ListNode.new
+            node = node.next
         end
-        node = node.next
     end
     l3
+end
+
+# Recursive solution
+def merge_two_lists(l1, l2)
+    return l1 if l2.nil?
+    return l2 if l1.nil?
+    
+    if l1.val <= l2.val
+        l1.next = merge_two_lists(l1.next, l2)
+        return l1
+    else
+        l2.next = merge_two_lists(l1, l2.next)
+        return l2
+    end
 end
