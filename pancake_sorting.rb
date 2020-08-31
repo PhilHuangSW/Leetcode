4c1a773ed480937f0f73423cb099a829ee09d219
Philip Huang <philip.sw.huang@gmail.com>
2020-08-30 21:27:49 -0700

    Finished Pancake Sorting Method and Problem

--- pancake_sorting.rb
+++ pancake_sorting.rb
@@ -43,37 +43,126 @@
 # @param {Integer[]} a
 # @return {Integer[]}
 def pancake_sort(a)
-  sorted = false
-  index = 0
-  while(a[index+1] != nil)
-		if a[index] > a[index+1]
-			a = reverse_swap(a,index)
-			index = 0
-			p a
-		else
-			index += 1
+	return [] if a.sort == a
+	ans = []
+	index = 0
+	largest = a[0]
+	size = (a.length-1)
+	# pancake sort method: 
+	# 1) Find largest number in array (or subset of array)
+	# 2) Flip it to the front (largest is in the front of the array)
+	# 3) Flip it to the back (so largest is in the back of array now)
+	# 4) Decrement the size of your array (since the largest is in the back now)
+	# 5) Continue until array is sorted
+	while(size != 0)
+		for i in 1..size
+			if a[i] > largest
+				largest = a[i]
+				index = i
+			end
 		end
+		# if largest is in front, flip it so that it is in the back
+		if index == 0
+			a = reverse_swap(a, size)
+			ans << size+1
+		# if anywhere else other than the back, flip it to the front
+		elsif index != size
+			a = reverse_swap(a, index)
+			ans << index+1
+			a = reverse_swap(a, size)
+			ans << size+1
+		end
+		# reseting and making a smaller subset of the array
+		largest = a[0]
+		index = 0
+		size -= 1
+	end
+	p a
+	ans
+end
+
+def reverse_swap(arr, k)
+	new_arr = []
+	leftover = (arr.size-1) - k
+
+	if k >= 1
+		for i in k.downto(0)
+			new_arr << arr[i]
+		end
+	end
+
+	if leftover == 1
+		new_arr << arr[(arr.size-1)]
+	elsif leftover >= 2
+		for i in k+1...arr.size
+			new_arr << arr[i]
+		end
+	end
+	new_arr
+end
+
+
+## Pancake sorting method
+# @param {Integer[]} a
+# @return {Integer[]}
+def pancake_sort(a)
+	p "start: #{a}"
+	index = 0
+	largest = a[0]
+	size = (a.length-1)
+	while(size != 0)
+		for i in 1..size
+			if a[i] > largest
+				largest = a[i]
+				index = i
+			end
+		end
+		if index == 0
+			a = reverse_swap(a, size)
+		elsif index != size
+			a = reverse_swap(a, index)
+			a = reverse_swap(a, size)
+		end
+		largest = a[0]
+		index = 0
+		size -= 1
 	end
 	a
 end
-      
+
 def reverse_swap(arr, k)
-  new_arr = []
-  return new_arr = arr.reverse if k == 0
-	
-  for i in 0..k
-    new_arr << arr[i]
+	new_arr = []
+	leftover = (arr.size-1) - k
+
+	if k >= 1
+		for i in k.downto(0)
+			new_arr << arr[i]
+		end
 	end
-	new_arr = new_arr.reverse
-  for i in k+1...arr.length
-    new_arr << arr[i]
-	end   
-	p new_arr
-  new_arr
+
+	if leftover == 1
+		new_arr << arr[(arr.size-1)]
+	elsif leftover >= 2
+		for i in k+1...arr.size
+			new_arr << arr[i]
+		end
+	end
+	# p "new_arr:  #{new_arr}"
+	new_arr
 end
 
 a = [3,2,4,1]
 b = [1,2,3]
 c = [5,2,8,1,3]
+d = [3,2,1]
+e = [5,4,3,2,1]
+f = [5,3,4,1,2]
+g = [21,2,5,3,30,1]
 
 p pancake_sort(a)
+p pancake_sort(b)
+p pancake_sort(c)
+p pancake_sort(d)
+p pancake_sort(e)
+p pancake_sort(f)
+p pancake_sort(g)
